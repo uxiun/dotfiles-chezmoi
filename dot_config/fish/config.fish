2045bfa7981f26baa538c147694e5915fe1b3a53
uxiun <teppeiito0905@gmail.com>
2024-09-27 08:54:49 +0900

    initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,27 @@
+abbr -a dor dotnet run
+abbr -a do dotnet
+abbr -a g git
+abbr -a m mkentries
+abbr -a le moar
+abbr -a ll ls -l
+abbr -a la ls -la
+abbr -a j cd
+abbr -a zm zellij -s
+abbr -a relogin exec /bin/fish -l
+abbr -a confi --set-cursor "echo \"%\" >> ~/.config/fish/config.fish"
+abbr -a -- dor 'dotnet run'
+abbr -a -- do dotnet
+abbr -a -- g git
+abbr -a -- m mkentries
+abbr -a -- le moar
+abbr -a -- ll 'ls -l'
+abbr -a -- la 'ls -la'
+abbr -a -- j cd
+abbr -a -- zm 'zellij -s'
+abbr -a -- relogin 'exec /bin/fish -l'
+abbr -a --set-cursor='%' -- confi 'echo "%" >> ~/.config/fish/config.fish'
+abbr -a aba abbr -a
+abbr -a coa --set-cursor "echo 'abbr -a %' >> ~/.config/fish/config.fish"
+set -g GUIX_LOCPATH /home/u/.guix-profile/lib/locale
+abbr -a , --set-cursor --position anywhere '~/%'
+abbr -a pas sudo pacman -S
